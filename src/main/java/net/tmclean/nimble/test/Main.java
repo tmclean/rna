d086d74f193d4fde0507b46af12bcd236903dae7
Tom McLean <tmclean@feith.com>
2013-12-10 20:50:52 -0500

    Adding slf4j support with optional runtime log4j implementation

--- src/main/java/net/tmclean/nimble/test/Main.java
+++ src/main/java/net/tmclean/nimble/test/Main.java
@@ -1,12 +1,18 @@
 package net.tmclean.nimble.test;
 
+import org.apache.log4j.PropertyConfigurator;
+
 public class Main 
 {
 	private static final String DEPLOY_URL = "http://localhost:9000";
 	
+	private static SyntheticServer server = null;
+	
 	public static void main( String[] args ) throws Throwable
 	{
-		SyntheticServer server = new SyntheticServer( DEPLOY_URL );
+		PropertyConfigurator.configure( Main.class.getResourceAsStream( "log4j.properties" ) );
+		
+		server = new SyntheticServer( DEPLOY_URL );
 		server.start();
 	}
 }
